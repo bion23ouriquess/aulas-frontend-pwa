64a895aeb459da3d9f8bec845971d41ff56779b5
GABRIEL OURIQUES BION <gabriel_bion@estudante.sesisenai.org.br>
2024-09-19 22:07:08 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <header>Cabeçalho</header>
+    <aside>Lado Esquerdo</aside>
+    <main>Principal</main>
+    <footer>Rodapé</footer>
+    </>
+  )
+}
+
+export default App
