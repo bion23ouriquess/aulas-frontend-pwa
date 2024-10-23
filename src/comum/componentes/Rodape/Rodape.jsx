3a471d10e00f70f01278a728738db31594c1224c
bion23ouriquess <gaby8.bion@gmail.com>
2024-10-23 20:21:17 -0300

    Ativando site de novo

--- src/comum/componentes/Rodape/Rodape.jsx
+++ src/comum/componentes/Rodape/Rodape.jsx
@@ -4,7 +4,7 @@ function Rodape() {
     const anoAtual = new Date().getFullYear();
 
     return <footer className="rodape_root">
-        Copyright © {anoAtual} - Todos os direitos reservados - Gabriel O. Bion.
+        Copyright © {anoAtual} - Todos os direitos reservados
     </footer>;
 }
 
